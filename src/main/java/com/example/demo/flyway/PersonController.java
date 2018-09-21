627ca5f14ff6f8bc03e96898e4b7a74f717ae2e5
Azam Afzaal <azam-afzaal.tahir@capgemini.com>
2018-09-21 09:48:08 +0200

    Added few changes

--- src/main/java/com/example/demo/flyway/PersonController.java
+++ src/main/java/com/example/demo/flyway/PersonController.java
@@ -6,8 +6,11 @@ import java.util.Optional;
 
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,6 +31,19 @@ public class PersonController {
 	public Optional<Person> readOne(@PathVariable Long id) {
 		LOG.info("Displaying one person entry");
 		return this.personRepository.findById(id);
+	}
+
+	@PostMapping(value = "/persons")
+	public Person saveOne(@RequestBody Person person) {
+		LOG.info("Saving one person entry");
+		return this.personRepository.save(person);
+	}
+
+	@DeleteMapping(value = "/persons/{id}")
+	public List<Person> deleteOne(@PathVariable Long id) {
+		LOG.info("Deleting one person entry");
+		this.personRepository.deleteById(id);
+		return this.personRepository.findAll();
 	}
 
 }
